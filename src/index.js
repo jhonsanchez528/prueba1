97132908ff58600bd79d0b375ef9b8bd61368bc3
jhonsanchez528 <jhonessanchez528@gmail.com>
2021-03-29 22:52:01 -0500

    Vista

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import reportWebVitals from './reportWebVitals';
 function tick() {
   const element = (
     <div>
-      <h1>Hello!</h1>
+      <h1>Hellos!</h1>
       <button>Este boton</button>
       <h2>It is {new Date().toLocaleDateString()}, {new Date().toLocaleTimeString()}.</h2>
     </div>
